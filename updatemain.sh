1de47efc9dffa2f33b5e496e1590cd5f7b61aeb4
carnivuth <matti200042@gmail.com>
2023-07-08 10:30:15 +0200

    update script

--- updatemain.sh
+++ updatemain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 if [[ $# -ne 1 ]];then exit 1; fi
-git switch "$1" && mkdir "../$1" && cp -r * "../$1/" && git switch main && rm -r "$1" && mv "../$1" . 
+git switch "$1" && mkdir "../$1" && cp -r * "../$1/" && git switch main && rm -r "$1" && mv "../$1" . && rm -r "../$1" 
