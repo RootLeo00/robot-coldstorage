7821408cf1ceb55e04193dae863e7523eb91e5ad
RootLeo00 <caterina.leonelli@gmail.com>
2023-07-20 17:45:10 +0200

    sprint3: remove dir

--- sprint3/unibolibs/unibo/basicomm23/interfaces/IContextMsgHandler.java
+++
@@ -1,8 +0,0 @@
-package unibo.basicomm23.interfaces;
-
-
-public interface IContextMsgHandler extends IApplMsgHandler {
-	public void addComponent( String name, IApplMsgHandler h);
-	public void removeComponent( String name );
-	public IApplMsgHandler getHandler( String name );
-}
