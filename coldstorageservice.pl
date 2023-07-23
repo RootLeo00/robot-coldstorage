164df0626c6fa7caf533c2d14d34cb72771ec0d3
RootLeo00 <caterina.leonelli@gmail.com>
2023-07-23 14:37:18 +0200

    sprint4: test sprint3.2 on rpi

--- coldstorageservice.pl
+++ coldstorageservice.pl
@@ -7,7 +7,7 @@ context(ctxcoldstorageservice, "localhost",  "TCP", "8038").
   qactor( coldstorageservice, ctxcoldstorageservice, "it.unibo.coldstorageservice.Coldstorageservice").
   qactor( coldroom, ctxcoldstorageservice, "it.unibo.coldroom.Coldroom").
   qactor( transporttrolley, ctxcoldstorageservice, "it.unibo.transporttrolley.Transporttrolley").
-  qactor( sonar, ctxcoldstorageservice, "sonarSimulator").
+  qactor( sonar, ctxcoldstorageservice, "sonarHCSR04Support23").
   qactor( datacleaner, ctxcoldstorageservice, "rx.dataCleaner").
   qactor( distancefilter, ctxcoldstorageservice, "rx.distanceFilter").
   qactor( sonar, ctxcoldstorageservice, "it.unibo.sonar.Sonar").
