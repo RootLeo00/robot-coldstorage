f7d91f4a44d87f7972550fe5546b5a1b0fa16544
RootLeo00 <caterina.leonelli@gmail.com>
2023-08-19 01:14:07 +0200

    sprint1: fix filepath config json; max weight read from json

--- resources/DomainSystemConfig.kt
+++ resources/DomainSystemConfig.kt
@@ -1,24 +1,33 @@
 import org.json.JSONObject
+import unibo.basicomm23.utils.ColorsOut
 import java.io.File
 import java.io.FileNotFoundException
 
 object DomainSystemConfig {
 
-	private var Expirationtime : Long = 10000;
+	private var Expirationtime : Long = 0;
+    private var Maxweight : Long = 0;
+
 
     init {
         try {
-            val config = File("../AppConfig.json").readText(Charsets.UTF_8)
+            val config = File("AppConfig.json").readText(Charsets.UTF_8)
             val jsonObject   =  JSONObject( config );
 
-
             Expirationtime= jsonObject.getLong("Expirationtime")
+            Maxweight= jsonObject.getLong("Maxweight")
+
         } catch (e : Exception) {
             println(" ${this.javaClass.name}  | ${e.localizedMessage}, activate simulation by default")
         }
     }
 
     fun getExpirationTime() : Long {
-        return Expirationtime
+        return Expirationtime;
     }
+
+    fun getMaxWeight() : Long {
+        return Maxweight;
+    }
+
 }
