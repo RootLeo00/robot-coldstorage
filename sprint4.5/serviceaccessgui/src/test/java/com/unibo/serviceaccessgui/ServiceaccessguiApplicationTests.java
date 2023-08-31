1edd738a908c6143893dacdff1541d2760e87a81
carnivuth <matti200042@gmail.com>
2023-08-31 16:40:08 +0200

    updated sprint4.5

---
+++ sprint4.5/serviceaccessgui/src/test/java/com/unibo/serviceaccessgui/ServiceaccessguiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibo.serviceaccessgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceaccessguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
